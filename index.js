827709cacbafecd7d6536b69b6656ab8a2cdaa30
Anthony Bronca <95654116+AnthonyBronca@users.noreply.github.com>
2023-01-07 03:00:57 -0500

    added random for array, strings, object, and case where no input is provided

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 function getRandomItem(el) {
     if (el instanceof Array) {
-        return Math.floor(Math.random() * el.length)
+        return el[Math.floor(Math.random() * el.length)]
+    } else if (typeof el === 'string') {
+        return el[Math.floor(Math.random() * el.length)]
+    } else if (el instanceof Object) {
+        const keys = Object.keys(el);
+
+        const key = keys[Math.floor(Math.random() * keys.length)];
+
+        return el[key]
+    }
+    else if (!el) {
+        return Math.floor(Math.random() * 100)
     }
 }
 
